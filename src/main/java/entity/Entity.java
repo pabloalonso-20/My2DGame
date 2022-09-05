c06cd3d803bde907f194e12fe75f2b3e81e6ad0c
pabloalonso-20 <alonso.pablo20@gmail.com>
2022-09-05 12:59:00 -0500

    Added walking animation to character

--- src/main/java/entity/Entity.java
+++ src/main/java/entity/Entity.java
@@ -10,4 +10,7 @@ public class Entity {
     // we use this to store our image files
     public BufferedImage up1, up2, down1, down2, left1, left2, right1, right2;
     public String direction;
+
+    public int spriteCounter =0;
+    public int spriteNum = 1;
 }
